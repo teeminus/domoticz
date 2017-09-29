1cb1b9f8423094610b4a27dc7f45a13c344d3901
Mr. Teeminus <32164856+teeminus@users.noreply.github.com>
2017-09-29 09:15:43 +0200

    - Build domoticz package from git sources

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
-domoticz
-========
+#OpenMediaVault domoticz package
+Build the domoticz package for OVM from source.
 
+##Prerequisites
+sudo apt-get install build-essential -y
+sudo apt-get install cmake libboost-dev libboost-thread-dev libboost-system-dev libsqlite3-dev subversion curl libcurl4-openssl-dev libusb-dev libudev-dev zlib1g-dev libssl-dev git
+
+##Building
 ./build_domoticz to build package
--- build_domoticz.sh
+++ build_domoticz.sh
@@ -1,24 +1,29 @@
-#!/bin/sh
+#!/bin/bash
 
-case "$(/usr/bin/arch)" in
-  *arm*)
-    arch="armv7l"
-    ;;
-  *)
-    arch="x86_64"
-    ;;
-esac
-url="https://releases.domoticz.com/releases/release/domoticz_linux_${arch}.tgz"
-
-rm -rfv opt
+rm -rf opt
 mkdir -p opt/domoticz
 cd  opt/domoticz
 
-tmp_file="domoticz.tgz"
+git clone -b master https://github.com/domoticz/domoticz.git git
+cd git
+__v_domoticz=$(awk '/Version/ { print $2; }' History.txt | head -n1)
+echo "domoticz (${__v_domoticz}) stable; urgency=low" > ../../../debian/changelog
+echo "  * Update to ${__v_domoticz}" >> ../../../debian/changelog
+echo " -- OpenMediaVault Plugin Developers <plugins@omv-extras.org>  $(date -R)"  >> ../../../debian/changelog
 
-wget "${url}" -O ${tmp_file}
-tar xvzf "${tmp_file}"
+cmake -DCMAKE_BUILD_TYPE=Release .
+make
 
-rm -f ${tmp_file}
+cp -r Config scripts www domoticz domoticz.sh server_cert.pem updatebeta updaterelease ../
+cd ..
+
+rm -rf git
 cd ../../
+
+__v=$(./opt/domoticz/domoticz --help | awk '/Domoticz V/ { print $4; }');
+#__v=${__v:1};
+echo "domoticz (${__v:1}) stable; urgency=low" > debian/changelog
+echo "  * Update to ${__v:1}" >> debian/changelog
+echo " -- OpenMediaVault Plugin Developers <plugins@omv-extras.org>  $(date -R)"  >> debian/changelog
+
 fakeroot debian/rules clean binary
